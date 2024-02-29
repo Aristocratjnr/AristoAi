86e92d0756dd336d56cc10d9d90eba5e62e33af1
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-02-29 15:48:08 +0000

    .

--- aristo.py
+++ aristo.py
@@ -1,8 +1,8 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils import executor
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-API_TOKEN = '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'
+API_TOKEN = '6832344311:AAFfQGEJS1ttkPPMXv2sy4s3fj4rbGWIyGY'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -11,18 +11,41 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+# Load MistralAI Mixtral model and tokenizer
+model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map={"cuda": 0})
+
 # Command message handler
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
     This handler will be called when the user sends "/start" or "/help" command
     """
-    await message.reply("Hi!\n I'm AristoAi!\nPowered by aiogram.")
+    welcome_message = "Hi!\nI'm AristoAi!\nPowered by MistralAI Mixtral."
+    await message.reply(welcome_message)
 
 # Echo message handler
 @dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+async def chat_with_mistral(message: types.Message):
+    """
+    Handler to chat with the MistralAI Mixtral model
+    """
+    user_input = message.text
+
+    # Prepare input for MistralAI Mixtral model
+    chat_history = [
+        {"role": "user", "content": user_input},
+    ]
+
+    inputs = tokenizer.apply_chat_template(chat_history, return_tensors="pt").to("cuda")
+
+    # Generate response from MistralAI Mixtral model
+    outputs = model.generate(inputs, max_new_tokens=50)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    # Send the response to the user
+    await message.answer(response)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
